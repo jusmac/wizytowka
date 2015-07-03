2d05c6c27f8a0dd15cf35015417ef2de5d9177f0
jusmac <justyna.machalica@gmail.com>
2015-07-03 14:24:18 +0200

    first

---
+++ js/app.js
@@ -0,0 +1,62 @@
+var scroll = function(){ 
+	$('a[href="#main"]').on('click', function(event) {
+
+		var target = $( $(this).attr('href') );
+
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 2000);
+		}
+	});
+	$('a[href="#description"]').on('click', function(event) {
+
+		var target = $( $(this).attr('href') );
+
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 2000);
+		}
+	});
+	$('a[href="#skills"]').on('click', function(event) {
+
+		var target = $( $(this).attr('href') );
+
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 2000);
+		}
+	});
+	$('a[href="#portfolio"]').on('click', function(event) {
+
+		var target = $( $(this).attr('href') );
+
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 2000);
+		}
+	});
+	$('a[href="#contact"]').on('click', function(event) {
+
+		var target = $( $(this).attr('href') );
+
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 2000);
+		}
+	});
+};
+
+
+$(function(){
+	scroll();
+});
